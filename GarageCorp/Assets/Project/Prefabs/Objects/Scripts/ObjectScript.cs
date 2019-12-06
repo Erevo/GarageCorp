b1be722d10c3f2d34fbcb5f92755d474822d7b1a
Student <student@mirea.ru>
2019-12-06 11:15:57 +0300

    Added general functions

---
+++ GarageCorp/Assets/Project/Prefabs/Objects/Scripts/ObjectScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScript : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    [SerializeField] AudioSource audio;
+
+    void OnMouseDown()
+    {
+        anim.Play("Panelkaflex");
+    }
+}
